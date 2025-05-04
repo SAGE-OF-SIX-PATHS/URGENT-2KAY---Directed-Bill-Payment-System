82757d2426f00f4cab4b81918e41024f6cb0c401
Ken-zzy <nwakalokenechukwu@gmail.com>
2025-05-04 22:46:52 +0100

    test

--- backend/src/modules/app.module.ts
+++ backend/src/modules/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { BillController } from '../controllers/bill.controller';
 import { BillService } from '../service/billService';
+import { AppController } from '../controllers/app.controller';
 
 @Module({
   imports: [],
-  controllers: [BillController],
+  controllers: [BillController, AppController],
   providers: [BillService],
 })
 export class BillModule {}
