de9a6034da102d675c3050f545b66bcb522ecdc6
Egole Ozioma <112742229+Ozioma45@users.noreply.github.com>
2025-05-07 06:33:12 +0100

    Update passport.ts to accept role during google auth

--- backend/src/service/passport.ts
+++ backend/src/service/passport.ts
@@ -8,9 +8,11 @@ passport.use(new GoogleStrategy(
     clientID: process.env.GOOGLE_CLIENT_ID || "",
     clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
     callbackURL: process.env.GOOGLE_CALLBACK_URL || "",
+    passReqToCallback: true,
   },
-  async (accessToken, refreshToken, profile, done) => {
-    const user = await findOrCreateUser(profile);
+  async (req: any, accessToken, refreshToken, profile, done)  => {
+     const role = req.query.role || "benefactee"; 
+    const user = await findOrCreateUser(profile, role);
   return done(null, user);
   }
 ));
