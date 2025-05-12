37fbac11fecdf37925827355af6a7e2d90fe7b75
Ozioma45 <oziomaegole@gmail.com>
2025-05-12 13:59:07 +0100

    adjust request for ids

--- backend/src/dto/request/Request.dto.ts
+++ backend/src/dto/request/Request.dto.ts
@@ -1,8 +1,8 @@
 export interface CreateRequestDto {
     name: string;
     notes?: string;
-    priority?: 'LOW' | 'MEDIUM' | 'HIGH';
     supporterId?: string;
+    billIds: string[]; 
   }
 
   export interface GetRequestsDto {
--- backend/src/services/request.service.ts
+++ backend/src/services/request.service.ts
@@ -2,24 +2,39 @@ import { prisma } from "../lib/prisma";
 import { CreateRequestDto, GetRequestsDto } from "../dto/request/Request.dto";
 
 export const createRequest = async (dto: CreateRequestDto, requesterId: string) => {
-  const { name, notes, priority = 'MEDIUM', supporterId} = dto;
+  const { name, notes,  supporterId, billIds } = dto;
 
-  // Create the request (bundle)
-  const request = await prisma.request.create({
-    data: {
-      name,
-      notes,
-      priority,
-      requesterId,
-      supporterId
-    },
-    include: {
-      bills: true,
-      supporter: true,
+  // Fetch bills and ensure they belong to the user and are not already part of a request
+  const bills = await prisma.bill.findMany({
+    where: {
+      id: { in: billIds },
+      userId: requesterId,
+      requestId: null,
     },
   });
 
-  return request;
+  if (bills.length !== billIds.length) {
+    throw new Error("Invalid or already-requested bills");
+  }
+
+    // Create the request (bundle)
+    const request = await prisma.request.create({
+      data: {
+        name,
+        notes,
+        requesterId,
+        supporterId,
+        bills: {
+          connect: billIds.map(id => ({ id })),
+        },
+      },
+      include: {
+        bills: true,
+        supporter: true,
+      },
+    });
+
+    return request;
 };
 
 
